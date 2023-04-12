0b69c5fe2c4b40697b1272a7d64a4eee2551074e
nico-ross <hustle.ross@yahoo.com>
2023-04-13 03:47:41 +0400

    2.1 homework summary

---
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileHeaderView.swift
+//  Navigation
+//
+//  Created by n p on 12.04.2023.
+//
+
+import UIKit
+
+class ProfileHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
